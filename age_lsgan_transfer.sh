e0b26b39074e4b113b1e56cd537d2a21f00a83af
David WANG <dawei6875797@163.com>
2018-06-15 15:24:17 +0800

    Update age_lsgan_transfer.sh

--- age_lsgan_transfer.sh
+++ age_lsgan_transfer.sh
@@ -1,8 +1,7 @@
-python mp_test.py \
+python test.py \
   --gan_loss_weight=75 \
   --fea_loss_weight=0.5e-4 \
   --age_loss_weight=30 \
   --fea_layer_name=conv5 \
-  --checkpoint_dir=/new_disk2/wangzw/tensorflow_models/292000/resnet_generator/age/0_conv5_lsgan_transfer_g75_0.5f-4_a30/ \
-  --sample_dir=age/0_conv5_lsgan_transfer_g75_0.5f-4_a30 \
-  --source_checkpoint_dir=/new_disk2/wangzw/tensorflow_models/292000/resnet_generator/age/0_conv5_lsgan_transfer_g75_0.5f-4_a20/
+  --checkpoint_dir=./checkpoints/ \
+  --sample_dir=age/0_conv5_lsgan_transfer_g75_0.5f-4_a30 
